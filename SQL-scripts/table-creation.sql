bdf7d631aab2a7c4360f7a6e698a648a99ea3f33
blakeaolson <blakeaolson1@gmail.com>
2023-10-02 17:55:36 -0500

    created orders table

--- SQL-scripts/table-creation.sql
+++ SQL-scripts/table-creation.sql
@@ -1,6 +1,14 @@
-CREATE TABLE Employees {
-  "ID" int,
-  "Name" varchar(255),
-  "Salary" int,
-  "Role", int
-}
+CREATE TABLE Employees (
+  ID int,
+  Name varchar(255),
+  Salary int,
+  Role int,
+);
+
+CREATE TABLE Orders (
+  ID int,
+  TotalAmount int,
+  Date varchar(255),
+  CashierName varchar(255),
+  PaymentMethod varchar(255),
+);
