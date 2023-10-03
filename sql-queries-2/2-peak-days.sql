08751dd8fbf1aa7ae3a924b89135c86b3255fa80
SathvikYeruva <saths.y@gmail.com>
2023-10-03 12:40:07 -0500

    extra fix

--- sql-queries-2/2-peak-days.sql
+++ sql-queries-2/2-peak-days.sql
@@ -3,4 +3,4 @@ SELECT
     SUM(CAST(totalamount AS NUMERIC)), --Select the sum of the order totals by date
 FROM orders
 GROUP BY DATE(orderdate) --Group the orders by date
-ORDER BY SUM(totalamount) DESC LIMIT 2; --Order the top 2 dates by their order total in descending order
+ORDER BY SUM(CAST(totalamount AS NUMERIC)) DESC LIMIT 2; --Order the top 2 dates by their order total in descending order
