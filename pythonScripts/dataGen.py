56f71b5a6b826814df519b5cc87f8be567ec51d0
root <root@Kevin-Dell-XPS>
2023-10-02 19:54:25 -0500

    added new columns to dataset

--- pythonScripts/dataGen.py
+++ pythonScripts/dataGen.py
@@ -13,23 +13,37 @@ end_date = datetime(2023, 12, 31)
 # Create a list to store the sales data
 sales_data = []
 
+# Generate customer IDs (6-digit numbers)
+customer_ids = [random.randint(100000, 999999) for _ in range(300)]
+
+# Generate cashier salaries
+cashier_salaries = {
+    "Sally": round(random.uniform(50000, 70000), 2),
+    "Suzy": round(random.uniform(50000, 70000), 2),
+    "Bob": round(random.uniform(50000, 70000), 2),
+    "Brandon": round(random.uniform(50000, 70000), 2)
+}
+
 # Generate 52 weeks of sales data
 for week in range(1, 53):
     # Generate random sales data for each day in the week
     for _ in range(7):
         order_id = random.randint(1, 100000)
-        # Generate order total with higher probability in the range of $5 to $15
         order_total = round(random.triangular(5, 15, 10), 2)
         date = start_date.strftime("%Y-%m-%d")
         cashier_name = random.choice(cashier_names)
         payment_method = random.choice(payment_methods)
-        sales_data.append([order_id, order_total, date, cashier_name, payment_method])
+        customer_id = random.choice(customer_ids)
+        role = "cashier"
+        cashier_salary = cashier_salaries[cashier_name]
+        
+        sales_data.append([order_id, order_total, date, cashier_name, payment_method, customer_id, role, cashier_salary])
         start_date += timedelta(days=1)
 
 # Write the generated data to a CSV file
 with open('sales_data_2023.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Order ID', 'Order Total', 'Date', 'Cashier Name', 'Payment Method'])
+    writer.writerow(['Order ID', 'Order Total', 'Date', 'Cashier Name', 'Payment Method', 'Customer ID', 'Role', 'Cashier Salary'])
     writer.writerows(sales_data)
 
-print("Sales data for 2023 generated and saved to 'sales_data_2023.csv'")
+print("Sales data for 2023 with additional columns generated and saved to 'sales_data_2023.csv'")
