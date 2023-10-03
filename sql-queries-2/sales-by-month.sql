13bb049eab29abfa4ce8cf3c9a5278cef39280c4
SathvikYeruva <saths.y@gmail.com>
2023-10-03 13:41:03 -0500

    complete top 10 sale sql + comments

--- sql-queries-2/sales-by-month.sql
+++ sql-queries-2/sales-by-month.sql
@@ -1,9 +1,9 @@
 SELECT
-    EXTRACT(YEAR FROM DATE(orderdate)) AS ordYear,
-    EXTRACT(MONTH FROM DATE(orderdate)) AS ordMonth,
-    SUM(CAST(totalamount AS NUMERIC)) AS salesTotal
+    EXTRACT(YEAR FROM DATE(orderdate)) AS ordYear, /*Get year after changing type to date*/
+    EXTRACT(MONTH FROM DATE(orderdate)) AS ordMonth, /*Get month after changing type to date*/
+    SUM(CAST(totalamount AS NUMERIC)) AS salesTotal /*Get total per month after changing type to numeric*/
 FROM orders
-GROUP BY
+GROUP BY /*Group and order by date (year and month)*/
     ordYear,
     ordMonth
 ORDER BY
