f372b24a4b054b6ca0c7d483265a08d3fa2125df
shubhamkalyani <65512390+shubhamkalyani@users.noreply.github.com>
2023-10-03 15:07:08 -0500

    moved sql scripts into one folder

--- sql-queries-2/lowest-5-inventory.sql
+++
@@ -1,4 +0,0 @@
-SELECT * 
-FROM inventory
-ORDER BY CAST(quantity AS NUMERIC) ASC /*Order lowest inventory item*/
-LIMIT 5; /*Bottom 5*/
--- sql-queries-2/top-3-payment-methods.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    paymentmethod,
-    COUNT(paymentmethod) AS paymentUse /*get payment use totals*/
-FROM orders /*Order 3 most used payment methods*/
-GROUP BY paymentmethod
-ORDER BY paymentUse DESC
-LIMIT 3;
