7ece7d7a9ccd9534703aa0e109b368f0afa371fa
blakeaolson <blakeaolson1@gmail.com>
2023-10-10 15:00:52 -0500

    added view controller for maincashierview

---
+++ testPOS/src/CashierMainViewController.java
@@ -0,0 +1,36 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class CashierMainViewController {
+
+    @FXML
+    private Button finish_order_button;
+
+    @FXML
+    public void checkout() {
+        try {
+            // Load the Login.fxml file
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/CashierItemSelection.fxml"));
+
+            // Create a new Stage
+            Stage stage = new Stage();
+            stage.setTitle("ItemSelection");
+            stage.setScene(new Scene(root, 460, 354));
+            
+            // Close the current dashboard stage
+            Stage currentStage = (Stage) finish_order_button.getScene().getWindow();
+            currentStage.close();
+
+            // Show the login stage
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
