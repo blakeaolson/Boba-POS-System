8c20f63dcd0f7a0ab986d0b69fa63898ee753596
SathvikYeruva <saths.y@gmail.com>
2023-10-16 09:52:29 -0500

    made the sales report gui functional

--- testPOS/src/ManagerDashboardController.java
+++ testPOS/src/ManagerDashboardController.java
@@ -206,11 +206,34 @@ public class ManagerDashboardController {
         }
     }
 
-        @FXML
+    @FXML
     public void loadSalesReportInput() {
         try {
             // Load the Login.fxml file
             Parent root = FXMLLoader.load(getClass().getResource("fxml/SalesReportInput.fxml"));
+
+            // Create a new Stage
+            Stage stage = new Stage();
+            stage.setTitle("Sales Report");
+            stage.setScene(new Scene(root, 460, 354));
+            stage.setMaximized(true);
+
+            // Close the current dashboard stage
+            Stage currentStage = (Stage) logoutButton.getScene().getWindow();
+            currentStage.close();
+
+            // Show the login stage
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void generateSalesReport() {
+        try {
+            // Load the Login.fxml file
+            Parent root = FXMLLoader.load(getClass().getResource("fxml/SalesReportOutput.fxml"));
 
             // Create a new Stage
             Stage stage = new Stage();
